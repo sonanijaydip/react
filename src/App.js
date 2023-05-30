6c06d1812f6aa57e4e1912520b5fdd53729b65c4
sonanijaydip <sonanijaydip4@gmail.com>
2023-05-30 09:37:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,135 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3 align="center">RESULT : BSC SEMS[CBSC]</h3>
+      <table border="1" width="100%" align="center">
+
+        <tr>
+          <td colspan="10">Certified showing the number of marks obtained by<br></br>Shri/Smt./Kumari<br></br>of <b>SIR P. T. SARVAJANIK COLLEGE OD SCIENCE, SURAT</b><br></br>in each header of passing at the <b>BACHLOR OF SCIENCE(FIFTH SEMESTER)held in APRIL-2016</b>
+          </td>
+        </tr>
+        <tr>
+          <th rowspan="2">Student name</th>
+          <th colspan="3">Passing mark</th>
+          <th colspan="6">Mark obtained</th>
+        </tr>
+        <tr>
+          <td>External</td>
+          <td>Internal</td>
+          <td>Total</td>
+          <td>External</td>
+          <td>Internal</td>
+          <td>Total</td>
+          <td>GR</td>
+          <td>GP</td>
+          <td>credit</td>
+        </tr>
+        <tr>
+          <td>LANGUAGE THROUGH LITERATURE</td>
+          <td>50/18</td>
+          <td>20</td>
+          <td>70/25</td>
+          <td>19</td>
+          <td>(09)</td>
+          <td>28</td>
+          <td>E</td>
+          <td>5</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>PAPER VI</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PAPER VII</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PAPER VIII</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PAPER IX</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PAPER X</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PAPER XI</td>
+          <td colspan="3"></td>
+          <td colspan="3"></td>
+        </tr>
+        <tr>
+          <td>PHYSICS</td>
+          <td>300/108</td>
+          <td>120</td>
+          <td>420/151</td>
+          <td>(143)</td>
+          <td>(60)</td>
+          <td>(203)</td>
+          <td>E</td>
+          <td>S</td>
+          <td>12</td>
+        </tr>
+        <tr>
+          <td>PHYSICS PRACTICAL</td>
+          <td>120/43</td>
+          <td>60</td>
+          <td>180/65</td>
+          <td>(91)</td>
+          <td>(44)</td>
+          <td>(135)</td>
+          <td>B</td>
+          <td>8</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>ELECTRONICS</td>
+          <td>50/18</td>
+          <td>20</td>
+          <td>70/25</td>
+          <td>(26)</td>
+          <td>(10)</td>
+          <td>(36)</td>
+          <td>D</td>
+          <td>6</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>NATIONAL SERVICE SCHEME	</td>
+          <td colspan="8"></td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td colspan="10" height="18"></td>
+        </tr>
+        <tr>
+          <th align="right">Aggregate Total	</th>
+          <th align="left">520</th>
+          <th></th>
+          <th align="left">740</th>
+          <th align="left">279</th>
+          <th></th>
+          <th align="left">402</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+        <tr>
+          <th align="right">result</th>
+          <th colspan="9" align="left">PASS</th>
+        </tr>
+      </table>
     </div>
   );
 }
